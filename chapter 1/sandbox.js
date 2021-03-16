7cda14b8f1803f6ba4007a46bfa0e3cffe9ec362
surajidk <surajidk12@gmail.com>
2021-03-16 14:08:53 +0700

    full object literals

--- chapter 1/sandbox.js
+++ chapter 1/sandbox.js
@@ -5,17 +5,28 @@ let user = {
     age: 30,
     email: 'crystal@thenetninja.co.uk',
     location: 'berlin',
-    blogs: ['why mac & cheese rules', '10 things to make with marmite']
-  };
-  
-//   console.log(user.blogs);
-  
-  user.age = 22;
-//   console.log(user.age);
-  
-  console.log(user['name']);
-  user.name = 'ajidk';
-  console.log(user['name']);
-  console.log(typeof user);
-//   console.log(typeof user);
-  
+    blogs: [
+        {title: 'why mac & cheese rules', likes: 30},
+        {title: '10 things to make with marmite', likes: 50}
+    ],
+    login: () => console.log('the user logged in'),
+    logout: (username) => {
+        const days = new Date();
+        const h = days.getHours();
+        const m = days.getMinutes();
+        const s = days.getSeconds();
+        let time = `${h}:${m}:${s}`
+
+        console.log(`
+        the ${username} logged out
+        latest ${time}
+        `);
+    },
+
+    logBlogs() {
+        this.blogs.forEach(blog => console.log(`${blog.title} punya ${blog.likes}`));
+    }
+
+}
+
+user.logBlogs();
